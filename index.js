282829ad32d2b0566f9c8bf6ce1563b8e7d5c9ba
Jamie DeLong <jdlong1980@gmail.com>
2020-07-19 22:05:55 -0400

    continued work on server routes

--- index.js
+++ index.js
@@ -41,14 +41,25 @@ function showAlert(errors) {
 };
 
 function submitForm(formData) {
-    console.log(formData);
+    $.ajax({ url: serverPath + "/api/answers", method: "POST", data: formData })
+        .then(response => {
+            $("#wrapper").remove();
+            $("#submitted").text("Thank You for Your Submission");
+        })
+        .catch(err => {
+            showAlert("A server error occured. Please try again.")
+            console.log(err);
+        })
 };
 
 function getServerStatus() {
-    $.get(serverPath + "/api/server")
+    $.ajax({ url: serverPath + "/api/server", method: "GET" })
         .then(response => {
             console.log(response);
-        });
+        })
+        .catch(err => {
+            console.log(err);
+        })
 };
 
 $("#submit").on("click", function () {
@@ -64,7 +75,7 @@ $("#submit").on("click", function () {
         address: $("#address").val().trim(),
         city: $("#city").val().trim(),
         state: $("#state").val(),
-        zip: parseInt($("#zip").val().trim()),
+        zip: $("#zip").val().trim() === "" ? null : parseInt($("#zip").val().trim()),
         phone: $("#phone").val().trim(),
         email: $("#email").val().trim(),
         fav_color: $("#fav-color").val().trim(),
@@ -89,4 +100,38 @@ $("#submit").on("click", function () {
     validateForm(formData);
 });
 
+//Ping the heroku server to ensure it's up when submitting
+setInterval(getServerStatus, 60000);
 getServerStatus();
+
+$("#first-name").val("Jamie");
+$("#last-name").val("DeLong");
+$("#dob-month").val("February");
+$("#dob-day").val("20");
+$("#dob-year").val("1998");
+$("#grade").val("12th");
+$("#parent").val("Cynthia and Lee DeLong");
+$("#allergies").val("Peanuts");
+$("#address").val("8830 Dunes Ct. 12-307");
+$("#city").val("Kissimmee");
+$("#state").val("FL");
+$("#zip").val("34747");
+$("#phone").val("407-376-8799");
+$("#email").val("jdlong1980@gmail.com");
+$("#fav-color").val("Blue");
+$("#fav-candy").val("Junior Mints");
+$("#fav-icecream").val("Butter Pecan");
+$("#fav-music").val("Pop");
+$("#fav-artist").val("Madonna");
+$("#fav-song").val("Like a Virgin");
+$("#fav-book").val("Discovery of Witches");
+$("#fav-person").val("Maya Angelou");
+$("#fav-saying").val("God Willing and the Crick Don't Rise");
+$("#fav-drink").val("Diet Cranberry Ginger Ale");
+$("#fav-food").val("Mac N Cheese");
+$("#fav-fruit").val("Cherries");
+$("#fav-game").val("World of Warcraft");
+$("#fav-dance").val("The Carlton");
+$("#fav-clothes").val("T-Shirt and Jeans");
+$("#fav-style").val("Baggy");
+$("#fav-app").val("Facebook");
